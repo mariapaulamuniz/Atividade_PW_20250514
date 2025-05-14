8d17b46f93abd46e7b5b84386fa8e7cb9af33748
Ricardo Maroquio <maroquio@gmail.com>
2025-05-14 16:55:20 -0300

    base para a atividade

---
+++ data/insert_categorias.sql
@@ -0,0 +1,12 @@
+INSERT INTO categoria (id, nome) 
+VALUES 
+(1, 'Eletrônicos'),
+(2, 'Roupas'),
+(3, 'Livros'),
+(4, 'Móveis'),
+(5, 'Esportes'),
+(6, 'Beleza'),
+(7, 'Brinquedos'),
+(8, 'Alimentos'),
+(9, 'Ferramentas'),
+(10, 'Automóveis');
